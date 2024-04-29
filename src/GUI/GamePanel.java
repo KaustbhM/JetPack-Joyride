35f9bbd727f88d5ad4bec629d6675c7fcf5c6842
Shrihan-T <165822669+Shrihan-T@users.noreply.github.com>
2024-04-28 17:12:01 -0700

    Update GamePanel.java added repaint to action performed

--- src/GUI/GamePanel.java
+++ src/GUI/GamePanel.java
@@ -20,6 +20,7 @@ public class GamePanel extends JPanel implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		repaint();
 		
 	}
 	

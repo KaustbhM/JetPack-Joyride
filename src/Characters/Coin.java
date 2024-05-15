af097fab5cc182f15f5acdf70aa2f75071c18c11
1096649 <1096649@NCHS120490480.ms.nsd.org>
2024-05-15 09:38:40 -0700

    cvergv

--- src/Characters/Coin.java
+++ src/Characters/Coin.java
@@ -8,9 +8,8 @@ public class Coin{
 	private int y;
 	private int width;
 	private int height;
-	//valid keeps track of whether the player hit the obstacle or not
-	//it is able to change by sending special values to contains method
-	private boolean valid = true;
+
+	private int xVelocity;
 	
 	public Coin(int id, int x, int y, int diameter) {
 		this.x = x;
@@ -24,6 +23,14 @@ public class Coin{
 		float distance = (float) (Math.sqrt(Math.pow(x - x1, 2) + Math.pow(y - y1, 2)));
 		return distance;
 
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
 	}
 
 

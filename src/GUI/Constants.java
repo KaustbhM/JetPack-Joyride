b49c854ffa3933aca3f632dc0f776cfca3bb214b
1096649 <1096649@NCHS120490480.ms.nsd.org>
2024-05-29 08:54:33 -0700

    New bat images + bat class

--- src/GUI/Constants.java
+++ src/GUI/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
   public static final int INIT_DIFFICULTY = 1;
   public static final int FINAL_DIFFICULTY = 6;
 
-  public static final String[] OBSTACLE_NAMES = {"Block", "Rocket"};
+  public static final String[] OBSTACLE_NAMES = {"Block", "Bat"};
   public static final int[] OBSTACLE_NUMBERS = { 1,2,3,4};
   public static final String[] OBSTACLE_FORMATION = {"Normal", "Diamond"};
 
